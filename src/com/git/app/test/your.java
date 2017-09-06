215561ad5695069e80530508e3d6e8799260a19c
triandripp <triandripp@gmail.com>
2017-09-06 15:25:12 +0700

    test conflict

--- src/com/git/app/test/your.java
+++ src/com/git/app/test/your.java
@@ -1,7 +1,8 @@
-package com.git.test;
+package com.git.app.test;
 
 public class your {
-public static void main (String [] args){
-}
+	public static void main(String[] args) {
+		System.out.println("asdasd");
+	}
 
 }
